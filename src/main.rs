81aeb81f65ac28e28998a505f2c86d271ddf26a6
t0wt13m5 <t0wt13m5@gmail.com>
2025-10-18 21:19:32 +0300

    Code cleanup and v.change updated to app v.0.1.3 removed testing code from main

--- src/main.rs
+++ src/main.rs
@@ -24,11 +24,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     controllers::database::path_handling::check_if_db_path_exists();
 
-    // TESTING STARTS HERE
-    // let test = controllers::database::path_handling::get_local_data_directory();
-    // println!("{:?}", test);
-    // TESTING ENDS HERE
-
     match cli.command {
         Commands::Fetch(args) => {
             args::fetch::execute(args).await?;
